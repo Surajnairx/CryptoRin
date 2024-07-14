a1c9bb21e9c71a5c4fa9d78892605271b7fd7486
Rinzler <Rinzler@123>
2024-07-14 22:15:03 +0530

    Updated tailwind.config file with fontFamily,fontSize and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: { poppins: "poppins" },
+    },
+    colors: {
+      gray: { 100: "#808080", 200: "#323232", 300: "#212121" },
+      white: "#fff",
+      cyan: "#14ffec",
+      red: "#d6436e",
+      green: "#25da72",
+    },
+    fontSize: {
+      sm: "14px",
+      md: "18px",
+      lg: "24px",
+      xl: "32px",
+      base: "16px",
+    },
   },
   plugins: [],
 };
