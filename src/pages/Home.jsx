5f3d38560ed59c694e704005e7f4902bf644db4a
Rinzler <Rinzler@123>
2024-07-16 21:08:29 +0530

    Created the Layout,Logo and Navigation Component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
 
 function Home() {
   return (
-    <div>
-      Home
+    <main className="w-full h-full flex flex-col content-center items-center relative text-white font-poppins">
+      <div className=" w-screen h-screen bg-gray-300 fixed -z-10"></div>
+      <Logo />
+      <Navigation />
       <Outlet />
-    </div>
+    </main>
   );
 }
 
