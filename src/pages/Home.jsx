7d8a4ecb30c2006be9fa09f082beb5fecc83de2e
Suraj Nair <surajnair9698@gmail.com>
2024-10-07 17:49:03 +0530

    Added the Logo component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Logo from "../components/Logo";
 
 const Home = () => {
   return (
-    <main className=" w-full h-full flex flex-col content-center items-center relative ">
-      <div className="w-screen h-screen bg-gray-300 fixed -z-10"></div>
+    <main className=" w-full h-full flex flex-col content-center items-center relative">
+      <div className="w-screen h-screen bg-gray-300 fixed -z-10" />
+      <Logo />
+
       <Outlet />
     </main>
   );
