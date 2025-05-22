7e9002dff06de00bc320c4433086a8b6ddea83f2
Suraj Nair <surajnair9698@gmail.com>
2025-05-22 16:38:47 +0530

    Installing and Setting Up React Router

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: { nunito: "Nunito" },
+    },
+    colors: {
+      grey: { 100: "#808080", 200: "#323232", 300: "#212121" },
+      white: "#fff",
+      cyan: "#14ffec",
+      green: "#25da72",
+    },
+    fontSize: { sm: "14px", md: "18px", lg: "24px", xl: "32px", base: "16px" },
   },
   plugins: [],
 };
