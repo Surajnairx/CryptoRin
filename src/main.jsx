e8ae3cd811a06de1a7195476efa6b907a2e1f26f
Suraj Nair <surajnair9698@gmail.com>
2025-11-10 12:48:10 +0530

    feat: Create routes and child routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
-
+import Home from "./pages/Home.jsx";
+import Crypto from "./pages/Crypto.jsx";
+import Trending from "./pages/Trending.jsx";
+import Saved from "./pages/Saved.jsx";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    children: [
+      {
+        path: "/",
+        element: <Crypto />,
+      },
+      {
+        path: "/trending",
+        element: <Trending />,
+      },
+      {
+        path: "/saved",
+        element: <Saved />,
+      },
+    ],
+  },
+]);
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
