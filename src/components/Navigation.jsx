3825b70bf0e8f06ef34a7f3fdf1515c5e475db80
Suraj Nair <surajnair9698@gmail.com>
2025-11-10 17:44:44 +0530

    feat: engineered the navigation component with active state styling

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function Navigation() {
+  return (
+    <nav className="w-[40%] mt-7 flex justify-around align-middle border border-cyan-300 rounded-lg">
+      <NavLink
+        to="/"
+        className={({ isActive }) => {
+          return isActive
+            ? `bg-cyan-300 text-gray-800 w-full text-base text-center m-2.5 active:bg-cyan-300 active:text-gray-500
+        border-0 cursor-pointer capitalize rounded font-bold`
+            : `w-full text-base text-center m-2.5 bg-gray-900 text-gray-100 hover:text-cyan-300 active:bg-cyan-300 active:text-gray-500
+        border-0 cursor-pointer capitalize rounded font-bold`;
+        }}
+      >
+        Crypto
+      </NavLink>
+      <NavLink
+        to="/trending"
+        className={({ isActive }) => {
+          return isActive
+            ? `bg-cyan-300 text-gray-800 w-full text-base text-center m-2.5 active:bg-cyan-300 active:text-gray-500
+        border-0 cursor-pointer capitalize rounded font-bold`
+            : `w-full text-base text-center m-2.5 bg-gray-900 text-gray-100 hover:text-cyan-300 active:bg-cyan-300 active:text-gray-500
+        border-0 cursor-pointer capitalize rounded font-bold`;
+        }}
+      >
+        Trending
+      </NavLink>
+      <NavLink
+        to="/saved"
+        className={({ isActive }) => {
+          return isActive
+            ? `bg-cyan-300 text-gray-800 w-full text-base text-center m-2.5 active:bg-cyan-300 active:text-gray-500
+        border-0 cursor-pointer capitalize rounded font-bold`
+            : `w-full text-base text-center m-2.5 bg-gray-900 text-gray-100 hover:text-cyan-300 active:bg-cyan-300 active:text-gray-500
+        border-0 cursor-pointer capitalize rounded font-bold`;
+        }}
+      >
+        Saved
+      </NavLink>
+    </nav>
+  );
+}
+
+export default Navigation;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
 
 function Home() {
   return (
     <main className=" w-full h-full flex flex-col first-letter:capitalize items-center relative text-white ">
       <div className="w-screen h-screen bg-black fixed -z-10"></div>
+      <Logo />
+      <Navigation />
 
       <Outlet />
     </main>
