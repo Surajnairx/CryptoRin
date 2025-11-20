ecc7583342b54295c3bf74ecc237c5f0c83df6f4
Suraj Nair <surajnair9698@gmail.com>
2025-11-20 13:32:12 +0530

    chore: cosmetic changes

--- src/pages/Trending.jsx
+++ src/pages/Trending.jsx
@@ -6,7 +6,7 @@ import { Outlet } from "react-router-dom";
 function Trending() {
   const { coins } = useContext(TrendingContext);
   return (
-    <div className="w-[80%] h-full flex flex-col mt-16 mb-24 relative">
+    <div className="w-[80%] h-full flex flex-col mt-9 mb-24 relative">
       <div className="flex flex-wrap min-h-[60vh] py-8 justify-evenly mt-9 border border-gray-100  rounded">
         {coins &&
           coins.map((coin) => <TrendingCoin key={coin.id} coin={coin.item} />)}
